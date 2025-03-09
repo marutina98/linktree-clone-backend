e4f738e57008d24f242b087092706514fddd9180
marutina98 <martinamineo98@gmail.com>
2025-03-09 23:32:56 +0100

    Feat: prepared user route for deletion

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,8 +8,8 @@ const controller = new UserController(prisma);
 
 router.get('/', controller.getUsers.bind(controller));
 router.get('/:id', controller.getUser.bind(controller));
-
 router.post('/', controller.createUser.bind(controller));
 router.put('/', controller.updateUser.bind(controller));
+router.delete('/', controller.deleteUser.bind(controller));
 
 export default router;
