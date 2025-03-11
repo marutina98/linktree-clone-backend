8c01884970e7171e0cee9c7c3c10badff3caf2e6
marutina98 <martinamineo98@gmail.com>
2025-03-11 17:39:46 +0100

    Fix: fixed isGuest middleware

--- src/middlewares/is-guest.middleware.ts
+++ src/middlewares/is-guest.middleware.ts
@@ -1,14 +1,41 @@
 
+// Imports
+
+import { verify } from 'jsonwebtoken';
+
 // Interfaces & Types
 
 import { Request, Response } from 'express';
+import IEmail from './../interfaces/email.interface';
+import IError from '../interfaces/error.interface';
 
 // Check if a user is a guest by verifying that the
-// authorizationHeader is absent.
+// authorizationHeader and the token are both absent.
+// If the token is present, checks that it's valid,
+// if valid, the user is not a guest.
 
 export default async function isGuest(request: Request, response: Response, next: Function) {
   
   const authorizationHeader = request.headers.authorization ?? null;
-  return !authorizationHeader ? true : false;
+ 
+  if (!authorizationHeader) return next();
+
+  const token = authorizationHeader.split(' ')[1];
+
+  if (!token) return next();
+
+  try {
+
+      verify(token, 'JWT_SECRET') as IEmail;
+
+      // Throw an error if the token is valid
+      
+      const error = new Error('User is not a guest.') as IError;
+      error.status = 401;
+      throw error;
+
+    } catch (error: unknown) {
+      return next();
+    }
 
 }
--- src/routes/authentication.routes.ts
+++ src/routes/authentication.routes.ts
@@ -7,14 +7,15 @@ import AuthenticationController from '../controllers/authentication.controller';
 // Middleware
 
 import isAuthenticated from '../middlewares/is-authenticated.middleware';
+import isGuest from '../middlewares/is-guest.middleware';
 
 // Routes
 
 const router = express.Router();
 const controller = new AuthenticationController();
 
-router.post('/register', (request: Request, response: Response, next: Function) => controller.registerUser(request, response, next));
-router.post('/login', (request: Request, response: Response, next: Function) => controller.login(request, response, next));
+router.post('/register', isGuest, (request: Request, response: Response, next: Function) => controller.registerUser(request, response, next));
+router.post('/login', isGuest, (request: Request, response: Response, next: Function) => controller.login(request, response, next));
 router.post('/logout', isAuthenticated, (request: Request, response: Response, next: Function) => controller.logout(request, response, next));
 
 export default router;
