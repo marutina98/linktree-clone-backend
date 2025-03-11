ccec06e4a2d3dbbbe51c5fc30f104d570694ee4d
marutina98 <martinamineo98@gmail.com>
2025-03-11 17:28:20 +0100

    Feat: added isAuthenticated middleware to some routes

--- src/routes/link.routes.ts
+++ src/routes/link.routes.ts
@@ -1,15 +1,24 @@
+
+// Imports
+
 import express, { Request, Response } from 'express';
 import LinkController from '../controllers/link.controller';
+
+// Middleware
+
+import isAuthenticated from '../middlewares/is-authenticated.middleware';
+
+// Routes
 
 const router = express.Router();
 const controller = new LinkController();
 
 router.get('/', (request: Request, response: Response, next: Function) => controller.getLinks(request, response, next));
 router.get('/:id', (request: Request, response: Response, next: Function) => controller.getLink(request, response, next));
-router.post('/', (request: Request, response: Response, next: Function) => controller.createLink(request, response, next));
-router.put('/:id/move-up', (request: Request, response: Response, next: Function) => controller.moveLinkUp(request, response, next));
-router.put('/:id/move-down', (request: Request, response: Response, next: Function) => controller.moveLinkDown(request, response, next));
-router.put('/:id', (request: Request, response: Response, next: Function) => controller.updateLink(request, response, next));
-router.delete('/:id', (request: Request, response: Response, next: Function) => controller.deleteLink(request, response, next));
+router.post('/', isAuthenticated, (request: Request, response: Response, next: Function) => controller.createLink(request, response, next));
+router.put('/:id/move-up', isAuthenticated, (request: Request, response: Response, next: Function) => controller.moveLinkUp(request, response, next));
+router.put('/:id/move-down', isAuthenticated, (request: Request, response: Response, next: Function) => controller.moveLinkDown(request, response, next));
+router.put('/:id', isAuthenticated, (request: Request, response: Response, next: Function) => controller.updateLink(request, response, next));
+router.delete('/:id', isAuthenticated, (request: Request, response: Response, next: Function) => controller.deleteLink(request, response, next));
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,21 @@
+
+// Imports
+
 import express, { Request, Response } from 'express';
 import UserController from '../controllers/user.controller';
+
+// Middleware
+
+import isAuthenticated from '../middlewares/is-authenticated.middleware';
+
+// Routes
 
 const router = express.Router();
 const controller = new UserController();
 
 router.get('/', (request: Request, response: Response, next: Function) => controller.getUsers(request, response, next));
 router.get('/:id', (request: Request, response: Response, next: Function) => controller.getUser(request, response, next));
-router.put('/:id', (request: Request, response: Response, next: Function) => controller.updateUser(request, response, next));
-router.delete('/:id', (request: Request, response: Response, next: Function) => controller.deleteUser(request, response, next));
+router.put('/:id', isAuthenticated, (request: Request, response: Response, next: Function) => controller.updateUser(request, response, next));
+router.delete('/:id', isAuthenticated, (request: Request, response: Response, next: Function) => controller.deleteUser(request, response, next));
 
 export default router;
