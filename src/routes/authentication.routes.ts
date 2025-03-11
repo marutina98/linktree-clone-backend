dcb8d7b45ead1051a73f3f82e0f1cda6f895fbc4
marutina98 <martinamineo98@gmail.com>
2025-03-11 17:28:59 +0100

    Feat: added isAuthenticated middleware to logout route

--- src/routes/authentication.routes.ts
+++ src/routes/authentication.routes.ts
@@ -1,11 +1,20 @@
+
+// Imports
+
 import express, { Request, Response } from 'express';
 import AuthenticationController from '../controllers/authentication.controller';
+
+// Middleware
+
+import isAuthenticated from '../middlewares/is-authenticated.middleware';
+
+// Routes
 
 const router = express.Router();
 const controller = new AuthenticationController();
 
 router.post('/register', (request: Request, response: Response, next: Function) => controller.registerUser(request, response, next));
 router.post('/login', (request: Request, response: Response, next: Function) => controller.login(request, response, next));
-router.post('/logout', (request: Request, response: Response, next: Function) => controller.logout(request, response, next));
+router.post('/logout', isAuthenticated, (request: Request, response: Response, next: Function) => controller.logout(request, response, next));
 
 export default router;
