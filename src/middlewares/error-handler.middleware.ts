ff5c71a80d006e1815f6b9078b8e0ed8bbeb7e5d
marutina98 <martinamineo98@gmail.com>
2025-03-12 18:54:49 +0100

    Fix: fixed error handling in errorHandler middleware

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -4,5 +4,8 @@ import IError from '../interfaces/error.interface';
 
 export default function errorHandlerMiddleware(error: IError, request: Request, response: Response, next: Function) {
   const status = error.status || 500;
-  response.status(status).json({ message: `ERROR: ${error.message}` });
+  response.status(status).json({
+    message: `ERROR: ${error.message}`,
+    status: status
+  });
 }
